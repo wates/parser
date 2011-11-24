5951b23cc221c2ca6b66ea7b20e5221f2a3a9b9f
wates <ssyu@wate.jp>
2011-11-24 19:50:27 +0900

    append Or

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-clang++ --std=c++0x parser.cc -o go.exe -I.
+clang++ --std=c++0x parser.cc -o go.exe -I. -fno-rtti -fno-exception
--- parser.cc
+++ parser.cc
@@ -20,7 +20,7 @@ namespace parser
 
 	template<char C=0,char... rest>
 	struct CharHasAny
-		:public CharHasAny<rest...>
+		//:public CharHasAny<rest...>
 	{
 		static bool Equal(const char *&text)
 		{
@@ -40,7 +40,7 @@ namespace parser
 
 	template<char C,char ...rest>
 	struct Char
-		:public CharHasAny<C,rest...>
+		//:public CharHasAny<C,rest...>
 	{
 		static bool Parse(const char *&text)
 		{
@@ -55,7 +55,7 @@ namespace parser
 
 	template<char C=0,char...rest>
 	struct CharSequence
-		:public CharSequence<rest...>
+		//:public CharSequence<rest...>
 	{
 		static const int length=1+CharSequence<rest...>::length;
 		static bool Equal(const char *text)
@@ -77,7 +77,7 @@ namespace parser
 
 	template<char C=0,char...rest>
 	struct Text
-		:public CharSequence<C,rest...>
+		//:public CharSequence<C,rest...>
 	{
 		static bool Parse(const char *&text)
 		{
@@ -106,6 +106,80 @@ namespace parser
 		}
 	};
 
+	// ここまではプリミティブな式のみ 
+
+
+	// あってもなくてもよい。 
+	template <typename T>
+	struct Option
+		:virtual public T
+	{
+		bool Parse(const char *&text)
+		{
+			T::Parse(text);
+			return true;
+		}
+	};
+
+	// 0 個以上にヒット。あるだけ削る。 
+	template<typename t>
+	struct Any
+		:public t
+	{
+		bool Parse(const char *&text)
+		{
+			while(t::Parse(text));
+			return true;
+		}
+	};
+
+	// 1 個以上にヒット。 
+	template<typename t>
+	struct More
+		:public t
+	{
+		bool Parse(const char *&text)
+		{
+			if(t::Parse(text))
+			{
+				while(t::Parse(text));
+				return true;
+			}
+			return false;
+		}
+	};
+
+
+	// どれかにヒット 
+	struct stop{};
+
+	template<typename t=stop,typename...rest>
+	struct Or
+		:virtual public t
+		,virtual public Or<rest...>
+	{
+		bool Parse(const char *&text)
+		{
+			const char *src=text;
+			if(t::Parse(text))
+				return true;
+			text=src;
+			if(Or<rest...>::Parse(text))
+				return true;
+			text=src;
+			return false;
+		}
+	};
+	template<typename...rest>
+	struct Or<stop,rest...>
+	{
+		static bool Parse(const char *text)
+		{
+            UNUSED(text);
+			return false;
+		}
+	};
+
 }
 
 #include <stdio.h>
@@ -114,7 +188,8 @@ template<typename T>
 void print_result(const char *source)
 {
 	const char *from=source;
-	printf("\"%s\" as %d - read %d chars. left - \"%s\"\n",from,T::Parse(source),source-from,source);
+	T t;
+	printf("\"%s\" as %d - read %d chars. left - \"%s\"\n",from,t.Parse(source),source-from,source);
 }
 
 
@@ -154,6 +229,48 @@ int main()
 		print_result<az>("a");
 		print_result<az>("z");
 		print_result<az>("A");
+	}
+	{
+		typedef parser::Text<'f','o','o'> seq;
+		typedef parser::Option<seq> opt;
+		print_result<opt>("foofoofoo");
+	}
+	{
+		typedef parser::Text<'f','o','o'> seq;
+		typedef parser::Option<seq> opt;
+		print_result<opt>("vvfoofoofoo");
+	}
+	{
+		typedef parser::Text<'f','o','o'> seq;
+		typedef parser::Any<seq> any;
+		print_result<any>("foofoofoo");
+	}
+	{
+		typedef parser::Text<'f','o','o'> seq;
+		typedef parser::More<seq> more;
+		print_result<more>("foofoofoo");
+	}
+	{
+		typedef parser::Text<'f','o','o'> seq;
+		typedef parser::More<seq> more;
+		print_result<more>("vvfoofoofoo");
+	}
+	{
+		using namespace parser;
+		typedef Or<Range<'0','9'>
+						,Range<'a','z'>
+						,Range<'A','Z'> > numbet;
+
+		print_result<numbet>("a");
+		print_result<numbet>("z");
+		print_result<numbet>("Z");
+		print_result<numbet>("0");
+		print_result<numbet>(" ");
+		print_result<numbet>(",");
+
+		typedef Any<numbet> numbets;
+		print_result<numbets>("abcABC912--");
+		print_result<numbets>("==abcABC912==");
 	}
 
 
